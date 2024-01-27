19bc2225806d5ed3a84622e4c810bb65ec3daf27
parham <parham7521@gmail.com>
2024-01-27 15:46:15 +0330

    debug

--- src/actions/main-action.ts
+++ src/actions/main-action.ts
@@ -1,8 +1,8 @@
-import { API, dataHeaders, requestHttp } from "@/services/request-http";
+import { API, dataHeaders } from "@/services/request-http";
 
-export const getAllHoldingData = (merchantId:number) => {
-    dataHeaders['content-type'] = "application/json";
-    dataHeaders["Accept"] = "application/json";
-    dataHeaders["X-VIEWNAME"] = "default";
-  requestHttp(`${merchantId}/holding`, "get", dataHeaders)
-}
+// export const getAllHoldingData = (merchantId:number) => {
+//     dataHeaders['content-type'] = "application/json";
+//     dataHeaders["Accept"] = "application/json";
+//     dataHeaders["X-VIEWNAME"] = "default";
+//   requestHttp(`${merchantId}/holding`, "get", dataHeaders)
+// }
